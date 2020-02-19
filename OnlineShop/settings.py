61463689a0e227523b46f5ef361a54e478d3eff5
mohan <sunkamohan68@gmail.com>
2020-02-19 17:17:42 +0530

    inital commit

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -56,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # simplified static serving.
+                          # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.whiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'OnlineShop.urls'
